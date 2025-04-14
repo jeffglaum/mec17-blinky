2f3ffe89bbde3b807fbafd8982bea034dca67049
Jeff Glaum <jeffglaum@live.com>
2025-04-14 10:56:52 -0700

    remove scripts, add default flash routine in MEC172x YAML

--- b.bat
+++
@@ -1,2 +0,0 @@
-"c:\Program Files (x86)\GNU Arm Embedded Toolchain\10 2021.10\bin\arm-none-eabi-objcopy.exe" -O binary .\target\thumbv7em-none-eabihf\debug\mec17-blinky .\target\thumbv7em-none-eabihf\debug\mec17-blinky.bin
-
